a928d7f96ed6372dec4fbcf39ac5dda9cb4b9ab1
Srinivas, Thirumalaa <SrinivaT@advisory.com>
2014-12-15 14:06:58 -0600

    Update with lesson #8 and #9; also uncommented previous lessons.

--- program.js
+++ program.js
@@ -1,3 +1,41 @@
+// lesson #9
+
+var http = require('http'),
+	bl = require('bl'),
+	urls = [
+		process.argv[2],
+		process.argv[3],
+		process.argv[4]
+	],
+	urlContent = [],
+	count = 0;
+
+urls.forEach(function(url, index){
+	http.get(url, function(res) {
+		res.pipe(bl(function(err,data){
+
+			if (err) {
+				return console.error(err);
+			}
+
+			urlContent[index] = data.toString();
+
+			count++;
+
+			if (count === 3) {
+				printResult();
+			}
+		}));
+	});	
+});
+
+function printResult() {
+	urlContent.forEach(function(val, index) {
+		console.log(val);
+	});
+}
+
+
 // lesson #8
 
 var http = require('http'),
@@ -17,86 +55,86 @@ http.get(url, function(res) {
 });
 
 
-// // lesson #7
+// lesson #7
 
-// var http = require('http'),
-// 	url = process.argv[2];
+var http = require('http'),
+	url = process.argv[2];
 
-// http.get(url, function(res) {
-// 	res.setEncoding('utf8');
-// 	res.on('data', function(data) {
-// 		console.log(data)
-// 	});
-// 	res.on('error', console.error);
-// });
+http.get(url, function(res) {
+	res.setEncoding('utf8');
+	res.on('data', function(data) {
+		console.log(data)
+	});
+	res.on('error', console.error);
+});
 
 
-// // lesson #6
+// lesson #6
 
-// var mod = require('./mymodule'),
-// 	dir = process.argv[2],
-// 	ext = process.argv[3];
+var mod = require('./mymodule'),
+	dir = process.argv[2],
+	ext = process.argv[3];
 
-// mod(dir, ext, function(err, files){
-// 	if (err) {
-// 		console.log('something errored out ', err);
-// 	}
+mod(dir, ext, function(err, files){
+	if (err) {
+		console.log('something errored out ', err);
+	}
 
-// 	files.forEach(function(file) {
-// 		console.log(file);
-// 	})
+	files.forEach(function(file) {
+		console.log(file);
+	})
 	
-// })
+})
 
 
-// // lesson #5
+// lesson #5
 
-// var fs = require('fs'),
-// 	path = require('path'),
-// 	dir = process.argv[2],
-// 	ext = process.argv[3];
+var fs = require('fs'),
+	path = require('path'),
+	dir = process.argv[2],
+	ext = process.argv[3];
 
-// fs.readdir(dir, function(err, files) {
-// 	for (var i=0; i< files.length; i++) {
-// 		fileExt = path.extname(files[i]).split('.');
-// 		fileExt = fileExt[1];
-// 		if (fileExt === ext.toString()) {
-// 			console.log(files[i]);
-// 		}
-// 	}
-// })
+fs.readdir(dir, function(err, files) {
+	for (var i=0; i< files.length; i++) {
+		fileExt = path.extname(files[i]).split('.');
+		fileExt = fileExt[1];
+		if (fileExt === ext.toString()) {
+			console.log(files[i]);
+		}
+	}
+})
 
-// // lesson #4
+// lesson #4
 
-// var fs = require('fs'),
-//     buf;
+var fs = require('fs'),
+    buf;
 
-// fs.readFile(process.argv[2], 'utf8', function(err, data) {
-// 	console.log(data.split('\n').length - 1);
-// })
-
-
-// // lesson #3
-
-// var fs = require('fs'),
-//     buf = fs.readFileSync(process.argv[2]),
-//     arr = buf.toString().split('\n'),
-//     countNewLine = arr.length;
-
-// console.log(countNewLine - 1);
+fs.readFile(process.argv[2], 'utf8', function(err, data) {
+	console.log(data.split('\n').length - 1);
+})
 
 
-// // lesson #2
-// var count, 
-// 	sum = 0;
+// lesson #3
 
-// for (count = 2; count < process.argv.length; count++) {
-// 	sum += +process.argv[count];
-// }
+var fs = require('fs'),
+    buf = fs.readFileSync(process.argv[2]),
+    arr = buf.toString().split('\n'),
+    countNewLine = arr.length;
 
-// console.log(sum);
+console.log(countNewLine - 1);
 
 
-// // lesson #1
+// lesson #2
+var count, 
+	sum = 0;
 
-// console.log('HEllO WORLD');
+for (count = 2; count < process.argv.length; count++) {
+	sum += +process.argv[count];
+}
+
+console.log(sum);
+
+
+// lesson #1
+
+console.log('HEllO WORLD');
